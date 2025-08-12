36a9d3dc4d4c1ae9afb2f431e098a761d7e897bb
Armando Medina <113925592+armsves@users.noreply.github.com>
2025-08-12 14:29:13 +0200

    deploying to vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
+
+    config.externals.push({
+      '../open_vote_contracts/js-scripts/generateInscriptionProof': 'commonjs ../open_vote_contracts/js-scripts/generateInscriptionProof',
+    });
     return config;
   },
+    excludeFiles: (src) => {
+    if (src.includes('open_vote_contracts/js-scripts/')) return true;
+    return false;
+  }
 };
 
 module.exports = nextConfig;
