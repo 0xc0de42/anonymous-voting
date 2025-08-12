a06863026df5909f6050a13db1bd31b8d734379f
Armando Medina <113925592+armsves@users.noreply.github.com>
2025-08-12 14:46:19 +0200

    deploying to vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+    
+    // Exclude contract files from compilation
+    config.module.rules.push({
+      test: /\.ts$/,
+      include: [/open_vote_contracts/],
+      use: 'ignore-loader'
+    });
+
     return config;
   },
 };
